fd0aad4b67947f0551ff235d99e5bba0b0833225
gragemediatechnology <gragemediatechnology@gmail.com>
2024-12-20 15:23:51 +0700

    sasas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        parent::boot();
 
         // Pastikan URL root sesuai dengan konfigurasi app.url
      URL::forceRootUrl(config('app.url'));
